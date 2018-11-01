830a77c0b63ffd2eccae541892aaef2806806dec
nathan-lui <misodone@gmail.com>
2018-11-01 12:07:58 -0500

    GET and DELETE requests functioning

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
@@ -9,6 +10,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST REQUESTS
 app.post('/recipes', (req, res) => {
     var recipe = new Recipe({
         recipe_name: req.body.recipe_name
@@ -20,11 +22,61 @@ app.post('/recipes', (req, res) => {
     });
 });
 
+// GET REQUESTS
+app.get('/recipes', (req, res) => {
+    Recipe.find().then((recipes) => {
+        res.send({recipes});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
+// GET /recipes/id
 
+app.get('/recipes/:id', (req, res) => {
+    Recipe.find().then((recipes) => {
+        var id = req.params.id;
+
+        if(!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
+
+        Recipe.findById(id).then((recipe) => {
+            if (!recipe){
+                return res.status(404).send();
+            }
+            else {
+                res.send({recipe});
+            }
+        }).catch((e) => {
+            res.status(400).send();
+        })
+    });
+});
+
+// DELETE /recipes/id
+app.delete('/recipes/:id', (req, res) => {
+    Recipe.find().then((recipes) => {
+        var id = req.params.id;
+
+        if(!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
+
+        Recipe.findByIdAndRemove(id).then((recipe) => {
+            if (!recipe){
+                return res.status(404).send();
+            }
+            res.send({recipe});
+        }).catch((e) => {
+            res.status(400).send();
+        })
+    });
+});
 app.listen(3000, () => {
     console.log('Program running on port 3000')
 });
 
+module.exports = {app};
 
 
